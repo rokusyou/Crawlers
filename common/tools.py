72ef3c187f2aaaf5ef9eccaf1c8e82e348efa57c
rokusyou <uekiyusuke@uekiyuusukenoMacBook-Pro.local>
2020-03-02 23:26:02 +0900

    SeintSeiya

---
+++ common/tools.py
@@ -0,0 +1,43 @@
+import urllib.request as req
+from bs4 import BeautifulSoup
+from time import sleep
+import csv
+
+
+def write_log(file,str):
+    f_logout = open(file,'a')
+    f_logout.writelines(str + "\n")
+    f_logout.close()
+
+def write_info(f_name,info_list):
+	
+	f_out = open(f_name,'a')
+	writer = csv.writer(f_out,lineterminator='\n')
+	
+	try:
+		writer.writerow(info_list)
+
+	except UnicodeEncodeError as e:
+		pass
+	
+	finally:
+		f_out.close()
+	
+
+# Request URL & get html
+def get_soup(url,sleep_sec):
+
+    try:
+        # Not Local 
+        res = req.urlopen(url)
+        soup = BeautifulSoup(res,'html.parser')
+        return soup
+
+    except urllib.error.HTTPError as http_e:
+        print( http_e.code , http_e.reason, url)
+        log_str = str( http_e.code) + http_e.reason +  url
+        write_log(LOG_F, str(http_e.code)  )
+    except Exception as e:
+        print(e)
+    finally:
+        sleep(sleep_sec)
