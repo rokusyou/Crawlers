101e5403e6d1866a2cc6559f598e35c5331c1c3e
rokusyou <ueki@uridoki.co.jp>
2020-03-10 23:44:49 +0900

    get image_url

--- hobby_search/exe_crawler.py
+++ hobby_search/exe_crawler.py
@@ -19,6 +19,7 @@ tstamp = '%04d%02d%02d%02d%02d%02d' % (date.year, date.month ,date.day,date.hour
 ROOT_URL = "https://www.1999.co.jp"
 URL = ROOT_URL +  "/search?typ1_c=101&cat=&state=&sold=0&sortid=0&searchkey=%E8%81%96%E9%97%98%E5%A3%AB%E6%98%9F%E7%9F%A2&spage="
 OUT_F = 'data/figure_items_%s.csv' % tstamp
+OUT_IMG_F = 'data/figure_items_images%s.csv' % tstamp
 LOG_F  = r"./log/log_%s.log" % tstamp
 NUM_PER_PAGE = 40
 SLEEP_SEC =1
@@ -54,14 +55,58 @@ KEYs = [
     "銀魂",
     "天元突破グレンラガン",
     "ネプテューヌ",
-    "けものフレンズ"
+    "けものフレンズ",
+    "メイドインアビス",
+    "魔法少女まどか☆マギカ",
+    "ネコぱら",
+    "NEW GAME!",
+    "ノーゲーム・ノーライフ",
+    "セーラームーン",
+    "聖闘士星矢",
+    "ドラゴンボール",
+    "ワンピース",
+    "エヴァンゲリオン",
+    "仮面ライダー",
+    "ナルト",
+    "ホットトイズ",
+    "超合金",
+    "トランスフォーマー",
+    "Vocaloid",
+    "METAL BUILD",
+    "鬼滅の刃",
+    "デビルマン",
+    "ハガレン",
+    "Re:ゼロから始める異世界生活",
+    "Fate",
+    "艦隊これくしょん -艦これ-",
+    "ごちうさ",
+    "のんのんびより",
+    "ゾンビランドサガ",
+    "冴えない彼女の育てかた",
+    "この素晴らしい世界に祝福を！",
+    "とある魔術の禁書目録",
+    "物語シリーズ",
+    "THE IDOLM@STER",
+    "ラブライブ！",
+    "ガールズ・アンド・パンツァー",
+    "アイカツ！",
+    "バンドリ",
+    "からかい上手の高木さん",
+    "五等分の花嫁",
+    "ベアブリック",
+    "マクロス",
+    "ゼノブレイド",
+    "ゴブリンスレイヤー",
+    "幼女戦記",
+    "手品先輩",
+    "ペルソナ"
 ]
 
 if __name__ == '__main__':
     for key in KEYs:
         write_log(LOG_F,key)
-        if os.path.exists("./data/images/%s/" % key) == False:
-            os.mkdir("./data/images/%s/" % key)
+        #if os.path.exists("./data/images/%s/" % key) == False:
+        #    os.mkdir("./data/images/%s/" % key)
         search_key = urllib.parse.quote(key)
         URL = ROOT_URL +  "/search?typ1_c=101&cat=figure&state=&sold=0&sortid=0&searchkey={0}&spage=".format(search_key)
         write_log(LOG_F, "Search items page")
@@ -96,7 +141,9 @@ if __name__ == '__main__':
                 try:
                     for url in img_list_soup.findAll('a'):
                         img_show_url = url.get("href" )
-                        get_img(ROOT_URL + img_show_url, ROOT_URL , "./data/images/%s/%s/" % (key , produ_id), SLEEP_SEC)
+                        img_url = get_img_url(ROOT_URL + img_show_url , SLEEP_SEC)
+                        img_info = [produ_id, jan_code, ROOT_URL + img_url]
+                        write_info(OUT_IMG_F, img_info)
                 except Exception as e:
-                    write_log(LOG_F, "when executing get_img")
+                    write_log(LOG_F, "when executing get_img_url")
                     write_log(LOG_F, str(e) )
